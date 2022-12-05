ad12d0defe50fb36d053625f931a2fc22fc8415c
Andrey Malkovskiy <andrey.malkovskiy@outlook.com>
2022-12-06 00:33:45 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <omp.h>
+#include <thread>
+
+std::chrono::milliseconds timeout(250);
+
+void test(int first, int last)
+{
+#pragma omp for schedule(static) ordered
+    for (int i = first; i <= last; ++i) {
+        std::this_thread::sleep_for(timeout);
+        if (i % 2)
+        {
+#pragma omp ordered
+            printf_s("test1() iteration %d\n", i);
+        }
+    }
+}
+
+void test2(int iter)
+{
+#pragma omp ordered
+    printf_s("test2() iteration %d\n", iter);
+}
+
+int main( )
+{
+    int i;
+#pragma omp parallel
+    {
+        test(1, 10);
+#pragma omp for ordered
+        for (i = 0 ; i < 5 ; i++)
+            test2(i);
+    }
+}
